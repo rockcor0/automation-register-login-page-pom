cded89bd44e62f7f793a08aff917387ce8f060a5
Ricardo Delgado <ridel007@gmail.com>
2020-12-21 20:32:09 -0500

    Base class

--- src/test/java/com/rockcor/selenium/Base.java
+++ src/test/java/com/rockcor/selenium/Base.java
@@ -19,10 +19,27 @@ public class Base implements WebDriver{
 	
 	//Page object model con selenium web driver
 	
-	private final static String val = "";
+	private final static String URL = "http://demo.guru99.com/test/newtours/";
+	private final static By LOCATOR_REGISTER_LINK = By.linkText("REGISTER");
+	private final static By LOCATOR_REGISTER_XPATH = By.xpath("//a[@data-target='#modalUserLogin']");
+	private final static By LOCATOR_CLOSE_ADD = By.xpath("//div[@id='closeBtn']");
+	private final static By LOCATOR_REGISTER_PAGE_FOUND = By.xpath("//img[@src='images/mast_register.gif']");
+	private final static By LOCATOR_REGISTER_EMAIL = By.id("email");
+	private final static By LOCATOR_REGISTER_PASS = By.name("password");
+	private final static By LOCATOR_REGISTER_PASS_CONF = By.cssSelector("input[name='confirmPassword']");
+	private final static By LOCATOR_REGISTER_SUBMIT_BTN = By.name("submit");
+	
+	private final static By LOCATOR_HOME = By.linkText("Home");
+	private final static By LOCATOR_LOGIN_USER = By.name("userName");
+	private final static By LOCATOR_LOGIN_PASSWORD = By.name("password");
+	private final static By LOCATOR_LOGIN_BTN = By.name("submit");
+	
+	private final static String USERNAME = "qualityAdmin123!=";
+	private final static String PASSWORD = "qualityAdmin123!=";
 	
 	private WebDriver driver;
 
+	
 	public Base(WebDriver driver) {
 		this.driver = driver;
 	}
@@ -33,69 +50,61 @@ public class Base implements WebDriver{
 		return driver;
 	}
 
-	public void get(String url) {
-		// TODO Auto-generated method stub
-		
+	public void get() {
+		driver.get(URL);
 	}
+	
+	public void get(String url) {
+		driver.get(url);
+	}
+	
 
 	public String getCurrentUrl() {
-		// TODO Auto-generated method stub
-		return null;
+		return driver.getCurrentUrl();
 	}
 
 	public String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
+		return driver.getTitle();
 	}
 
-	public List<WebElement> findElements(By by) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<WebElement> findElements(By locator) {
+		return driver.findElements(locator);
 	}
 
-	public WebElement findElement(By by) {
-		// TODO Auto-generated method stub
-		return null;
+	public WebElement findElement(By locator) {
+		return driver.findElement(locator);
 	}
 
 	public String getPageSource() {
-		// TODO Auto-generated method stub
-		return null;
+		return driver.getPageSource();
 	}
 
 	public void close() {
-		// TODO Auto-generated method stub
-		
+		driver.close();
 	}
 
 	public void quit() {
-		// TODO Auto-generated method stub
-		
+		driver.quit();
 	}
 
 	public Set<String> getWindowHandles() {
-		// TODO Auto-generated method stub
-		return null;
+		return driver.getWindowHandles();
 	}
 
 	public String getWindowHandle() {
-		// TODO Auto-generated method stub
-		return null;
+		return driver.getWindowHandle();
 	}
 
 	public TargetLocator switchTo() {
-		// TODO Auto-generated method stub
-		return null;
+		return driver.switchTo();
 	}
 
 	public Navigation navigate() {
-		// TODO Auto-generated method stub
-		return null;
+		return driver.navigate();
 	}
 
 	public Options manage() {
-		// TODO Auto-generated method stub
-		return null;
+		return driver.manage();
 	}
 
 }
