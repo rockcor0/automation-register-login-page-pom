3b2dd5cf245aa43aaa27ab5f9b7b010ab37ae8aa
Ricardo Delgado <ridel007@gmail.com>
2020-12-26 11:58:21 -0500

    Login page

--- src/test/java/com/rockcor/selenium/LoginPage.java
+++ src/test/java/com/rockcor/selenium/LoginPage.java
@@ -10,68 +10,80 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class LoginPage extends Base{
-	
+
 	private final static By LOCATOR_HOME = By.linkText("Home");
 	private final static By LOCATOR_LOGIN_USER = By.name("userName");
 	private final static By LOCATOR_LOGIN_PASSWORD = By.name("password");
 	private final static By LOCATOR_LOGIN_BTN = By.name("submit");
 	private final static By LOCATOR_REGISTER_MESSAGE_SUCCESS = By.tagName("font");
 
-	
+
 	private final static String USERNAME = "qualityAdmin123!=";
 	private final static String PASSWORD = "qualityAdmin123!=";
-	
+
 	public final static String SING_IN_MESSAGE = "Thank you for Loggin.";
-	
+
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
 	}
-	
+
+	/**
+	 * User login
+	 */
 	public void loginUser() {
-		
-		if( isHomePageDisplayerd() ) {
-			
-		}
-		
+
 		click(LOCATOR_HOME);
-		
-		implicitlyWait(1000);
-		
-		sendKeys(USERNAME, LOCATOR_LOGIN_USER);
-		sendKeys(PASSWORD, LOCATOR_LOGIN_PASSWORD);
-		click(LOCATOR_LOGIN_BTN);
-		
-		implicitlyWait(1000);
-		
-		List<WebElement> fonts = findElements(LOCATOR_REGISTER_MESSAGE_SUCCESS);
-		
-		for(WebElement item : fonts) {
-			System.out.println(fonts.indexOf(item) + " - " + item);
+
+		if( isHomePageDisplayerd() ) {
+			implicitlyWait(1000);
+
+			sendKeys(USERNAME, LOCATOR_LOGIN_USER);
+			sendKeys(PASSWORD, LOCATOR_LOGIN_PASSWORD);
+			click(LOCATOR_LOGIN_BTN);
+
+			implicitlyWait(1000);
+
+			List<WebElement> fonts = findElements(LOCATOR_REGISTER_MESSAGE_SUCCESS);
+
+			for(WebElement item : fonts) {
+				System.out.println(fonts.indexOf(item) + " - " + item);
+			}
+
+			String result = "Thank you for Loggin.";
+
+			assertEquals(fonts.get(3).getText(), result);
+
+		} else {
+			System.out.print("Home page was not found");
 		}
-		
-		String result = "Thank you for Loggin.";
-		
-		assertEquals(fonts.get(3).getText(), result);
 	}
-	
+
+	/**
+	 * If user is in home page
+	 * @return
+	 */
 	public boolean isHomePageDisplayerd() {
 		return true;
 	}
-	
+
+	/**
+	 * Message when user signIn
+	 * @return
+	 */
 	public String signInMessage() {
-		
-List<WebElement> fonts = findElements(LOCATOR_REGISTER_MESSAGE_SUCCESS);
-		
+
+		List<WebElement> fonts = findElements(LOCATOR_REGISTER_MESSAGE_SUCCESS);
+
 		for(WebElement item : fonts) {
 			System.out.println(fonts.indexOf(item) + " - " + item);
 		}
-		
+
 		//String result = "Thank you for Loggin.";
 		//assertEquals(fonts.get(3).getText(), result);
-		
+
 		return getText(fonts.get(3));
-		
+
 	}
 
 }
